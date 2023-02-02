e4d909f0b49def88cf7c15c9f929cc3d402b203e
Vitor Butkus <vitorbutkus@gmail.com>
2023-02-01 21:07:11 -0300

    24 imgs

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const imgSrcs = [
   '24',
 ];
 
-for (i = 0; i <= 5; i++) {
+for (i = 0; i < 24; i++) {
   //   const newDiv = document.createElement('div');
   //   newDiv.classList.add('pic');
   //   newDiv.style.background = colors[i];
@@ -40,7 +40,7 @@ for (i = 0; i <= 5; i++) {
   //   }
   const newImg = document.createElement('img');
   newImg.classList.add('pic');
-  if (i < 10) {
+  if (i < 9) {
     newImg.src = `0${i + 1}.jpg`;
   } else {
     newImg.src = `${i + 1}.jpg`;
@@ -115,31 +115,278 @@ container.addEventListener('mouseenter', (e) => {
 container.addEventListener('mousemove', (e) => {
   const x = e.offsetX;
   const cWidth = container.offsetWidth;
+  const l = pics.length;
   console.log(x);
-  console.log(cWidth);
-  if (x > 0 && x < cWidth / 6) {
-    pics[0].style.opacity = scale(x, 0, cWidth / 6, 1, 0);
-    pics[1].style.opacity = scale(x, 0, cWidth / 6, 0, 1);
+  //   console.log(cWidth);
+  if (x > 0 && x < cWidth / l) {
+    pics[0].style.opacity = scale(x, 0, cWidth / l, 1, 0);
+    pics[1].style.opacity = scale(x, 0, cWidth / l, 0, 1);
   }
-  if (x >= cWidth / 6 && x < (cWidth / 6) * 2) {
-    pics[1].style.opacity = scale(x, cWidth / 6, (cWidth / 6) * 2, 1, 0);
-    pics[2].style.opacity = scale(x, cWidth / 6, (cWidth / 6) * 2, 0, 1);
+  if (x >= cWidth / l && x < (cWidth / l) * 2) {
+    pics[1].style.opacity = scale(x, cWidth / l, (cWidth / l) * 2, 1, 0);
+    pics[2].style.opacity = scale(x, cWidth / l, (cWidth / l) * 2, 0, 1);
   }
-  if (x >= (cWidth / 6) * 2 && x < (cWidth / 6) * 3) {
-    pics[2].style.opacity = scale(x, (cWidth / 6) * 2, (cWidth / 6) * 3, 1, 0);
-    pics[3].style.opacity = scale(x, (cWidth / 6) * 2, (cWidth / 6) * 3, 0, 1);
+  if (x >= (cWidth / l) * 2 && x < (cWidth / l) * 3) {
+    pics[2].style.opacity = scale(x, (cWidth / l) * 2, (cWidth / l) * 3, 1, 0);
+    pics[3].style.opacity = scale(x, (cWidth / l) * 2, (cWidth / l) * 3, 0, 1);
   }
-  if (x >= (cWidth / 6) * 3 && x < (cWidth / 6) * 4) {
-    pics[3].style.opacity = scale(x, (cWidth / 6) * 3, (cWidth / 6) * 4, 1, 0);
-    pics[4].style.opacity = scale(x, (cWidth / 6) * 3, (cWidth / 6) * 4, 0, 1);
+  if (x >= (cWidth / l) * 3 && x < (cWidth / l) * 4) {
+    pics[3].style.opacity = scale(x, (cWidth / l) * 3, (cWidth / l) * 4, 1, 0);
+    pics[4].style.opacity = scale(x, (cWidth / l) * 3, (cWidth / l) * 4, 0, 1);
   }
-  if (x >= (cWidth / 6) * 4 && x < (cWidth / 6) * 5) {
-    pics[4].style.opacity = scale(x, (cWidth / 6) * 4, (cWidth / 6) * 5, 1, 0);
-    pics[5].style.opacity = scale(x, (cWidth / 6) * 4, (cWidth / 6) * 5, 0, 1);
+  if (x >= (cWidth / l) * 4 && x < (cWidth / l) * 5) {
+    pics[4].style.opacity = scale(x, (cWidth / l) * 4, (cWidth / l) * 5, 1, 0);
+    pics[5].style.opacity = scale(x, (cWidth / l) * 4, (cWidth / l) * 5, 0, 1);
   }
-  if (x >= (cWidth / 6) * 5 && x < (cWidth / 6) * 6) {
-    pics[5].style.opacity = 1;
-    // pics[5].style.opacity = scale(x, (cWidth / 6) * 5, (cWidth / 6) * 6, 1, 0);
-    // pics[6].style.opacity = scale(x, (cWidth / 6) * 5, (cWidth / 6) * 6, 0, 1);
+  if (x >= (cWidth / l) * 5 && x < (cWidth / l) * 6) {
+    // pics[5].style.opacity = 1;
+    pics[5].style.opacity = scale(x, (cWidth / l) * 5, (cWidth / l) * 6, 1, 0);
+    pics[6].style.opacity = scale(x, (cWidth / l) * 5, (cWidth / l) * 6, 0, 1);
+  }
+  if (x >= (cWidth / l) * 6 && x < (cWidth / l) * 7) {
+    // pics[5].style.opacity = 1;
+    pics[6].style.opacity = scale(x, (cWidth / l) * 6, (cWidth / l) * 7, 1, 0);
+    pics[7].style.opacity = scale(x, (cWidth / l) * 6, (cWidth / l) * 7, 0, 1);
+  }
+  if (x >= (cWidth / l) * 7 && x < (cWidth / l) * 8) {
+    // pics[5].style.opacity = 1;
+    pics[7].style.opacity = scale(x, (cWidth / l) * 7, (cWidth / l) * 8, 1, 0);
+    pics[8].style.opacity = scale(x, (cWidth / l) * 7, (cWidth / l) * 8, 0, 1);
+  }
+  if (x >= (cWidth / l) * 8 && x < (cWidth / l) * 9) {
+    // pics[5].style.opacity = 1;
+    pics[8].style.opacity = scale(x, (cWidth / l) * 8, (cWidth / l) * 9, 1, 0);
+    pics[9].style.opacity = scale(x, (cWidth / l) * 8, (cWidth / l) * 9, 0, 1);
+  }
+  if (x >= (cWidth / l) * 9 && x < (cWidth / l) * 10) {
+    // pics[5].style.opacity = 1;
+    pics[9].style.opacity = scale(x, (cWidth / l) * 9, (cWidth / l) * 10, 1, 0);
+    pics[10].style.opacity = scale(
+      x,
+      (cWidth / l) * 9,
+      (cWidth / l) * 10,
+      0,
+      1
+    );
+  }
+  if (x >= (cWidth / l) * 10 && x < (cWidth / l) * 11) {
+    // pics[5].style.opacity = 1;
+    pics[10].style.opacity = scale(
+      x,
+      (cWidth / l) * 10,
+      (cWidth / l) * 11,
+      1,
+      0
+    );
+    pics[11].style.opacity = scale(
+      x,
+      (cWidth / l) * 10,
+      (cWidth / l) * 11,
+      0,
+      1
+    );
+  }
+  if (x >= (cWidth / l) * 11 && x < (cWidth / l) * 12) {
+    // pics[5].style.opacity = 1;
+    pics[11].style.opacity = scale(
+      x,
+      (cWidth / l) * 11,
+      (cWidth / l) * 12,
+      1,
+      0
+    );
+    pics[12].style.opacity = scale(
+      x,
+      (cWidth / l) * 11,
+      (cWidth / l) * 12,
+      0,
+      1
+    );
+  }
+  if (x >= (cWidth / l) * 12 && x < (cWidth / l) * 13) {
+    // pics[5].style.opacity = 1;
+    pics[12].style.opacity = scale(
+      x,
+      (cWidth / l) * 12,
+      (cWidth / l) * 13,
+      1,
+      0
+    );
+    pics[13].style.opacity = scale(
+      x,
+      (cWidth / l) * 12,
+      (cWidth / l) * 13,
+      0,
+      1
+    );
+  }
+  if (x >= (cWidth / l) * 13 && x < (cWidth / l) * 14) {
+    // pics[5].style.opacity = 1;
+    pics[13].style.opacity = scale(
+      x,
+      (cWidth / l) * 13,
+      (cWidth / l) * 14,
+      1,
+      0
+    );
+    pics[14].style.opacity = scale(
+      x,
+      (cWidth / l) * 13,
+      (cWidth / l) * 14,
+      0,
+      1
+    );
+  }
+  if (x >= (cWidth / l) * 14 && x < (cWidth / l) * 15) {
+    // pics[5].style.opacity = 1;
+    pics[14].style.opacity = scale(
+      x,
+      (cWidth / l) * 14,
+      (cWidth / l) * 15,
+      1,
+      0
+    );
+    pics[15].style.opacity = scale(
+      x,
+      (cWidth / l) * 14,
+      (cWidth / l) * 15,
+      0,
+      1
+    );
+  }
+  if (x >= (cWidth / l) * 15 && x < (cWidth / l) * 16) {
+    // pics[5].style.opacity = 1;
+    pics[15].style.opacity = scale(
+      x,
+      (cWidth / l) * 15,
+      (cWidth / l) * 16,
+      1,
+      0
+    );
+    pics[16].style.opacity = scale(
+      x,
+      (cWidth / l) * 15,
+      (cWidth / l) * 16,
+      0,
+      1
+    );
+  }
+  if (x >= (cWidth / l) * 16 && x < (cWidth / l) * 17) {
+    // pics[5].style.opacity = 1;
+    pics[16].style.opacity = scale(
+      x,
+      (cWidth / l) * 16,
+      (cWidth / l) * 17,
+      1,
+      0
+    );
+    pics[17].style.opacity = scale(
+      x,
+      (cWidth / l) * 16,
+      (cWidth / l) * 17,
+      0,
+      1
+    );
+  }
+  if (x >= (cWidth / l) * 17 && x < (cWidth / l) * 18) {
+    pics[17].style.opacity = scale(
+      x,
+      (cWidth / l) * 17,
+      (cWidth / l) * 18,
+      1,
+      0
+    );
+    pics[18].style.opacity = scale(
+      x,
+      (cWidth / l) * 17,
+      (cWidth / l) * 18,
+      0,
+      1
+    );
+  }
+  if (x >= (cWidth / l) * 18 && x < (cWidth / l) * 19) {
+    pics[18].style.opacity = scale(
+      x,
+      (cWidth / l) * 18,
+      (cWidth / l) * 19,
+      1,
+      0
+    );
+    pics[19].style.opacity = scale(
+      x,
+      (cWidth / l) * 18,
+      (cWidth / l) * 19,
+      0,
+      1
+    );
+  }
+  if (x >= (cWidth / l) * 19 && x < (cWidth / l) * 20) {
+    pics[19].style.opacity = scale(
+      x,
+      (cWidth / l) * 19,
+      (cWidth / l) * 20,
+      1,
+      0
+    );
+    pics[20].style.opacity = scale(
+      x,
+      (cWidth / l) * 19,
+      (cWidth / l) * 20,
+      0,
+      1
+    );
+  }
+  if (x >= (cWidth / l) * 20 && x < (cWidth / l) * 21) {
+    pics[20].style.opacity = scale(
+      x,
+      (cWidth / l) * 20,
+      (cWidth / l) * 21,
+      1,
+      0
+    );
+    pics[21].style.opacity = scale(
+      x,
+      (cWidth / l) * 20,
+      (cWidth / l) * 21,
+      0,
+      1
+    );
+  }
+  if (x >= (cWidth / l) * 21 && x < (cWidth / l) * 22) {
+    pics[21].style.opacity = scale(
+      x,
+      (cWidth / l) * 21,
+      (cWidth / l) * 22,
+      1,
+      0
+    );
+    pics[22].style.opacity = scale(
+      x,
+      (cWidth / l) * 21,
+      (cWidth / l) * 22,
+      0,
+      1
+    );
+  }
+  if (x >= (cWidth / l) * 22 && x < (cWidth / l) * 23) {
+    pics[22].style.opacity = scale(
+      x,
+      (cWidth / l) * 22,
+      (cWidth / l) * 23,
+      1,
+      0
+    );
+    pics[23].style.opacity = scale(
+      x,
+      (cWidth / l) * 22,
+      (cWidth / l) * 23,
+      0,
+      1
+    );
+  }
+  if (x >= (cWidth / l) * 23 && x < (cWidth / l) * 24) {
+    pics[23].style.opacity = 1;
+    // pics[23].style.opacity = scale(x, (cWidth / 6) * 23, (cWidth / 6) * 24, 1, 0);
+    // pics[24].style.opacity = scale(x, (cWidth / 6) * 23, (cWidth / 6) * 24, 0, 1);
   }
 });
